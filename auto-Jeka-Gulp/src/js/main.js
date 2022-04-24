0decea3574853a501137c29a20e849ee8e211702
leska-ver <lyaisan.333@yandex.ru>
2022-04-24 12:58:19 +0500

    C Gulp-ом

--- auto-Jeka-Gulp/src/js/main.js
+++ auto-Jeka-Gulp/src/js/main.js
@@ -2,7 +2,6 @@ import "inputmask";
 
 // import "just-validate";
 import "../../node_modules/just-validate/dist/js/just-validate.min.js";
-import projectHigh from "./components/projectHigh";
 import autoJeka from "./components/auto-Jeka";
 import search from "./components/search";
 import footerModal from "./components/footer-modal";
